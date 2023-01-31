9d4038af2b60e12e4f2e9821fc6d8c9d9d162cd5
tuhin637 <tuhin637bd@gmail.com>
2023-01-31 15:08:58 +0600

    2nd Hw Done

---
+++ Lab Task 1/SimpleCalculator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.labtask1;
+
+/**
+ *
+ * @author USER
+ */
+public class SimpleCalculator {
+    public static void main(String ars[]){
+        int a,b;
+        a=10;
+        b=50;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(b/a);
+        System.out.println(a*b);
+    }
+    
+    
+}
