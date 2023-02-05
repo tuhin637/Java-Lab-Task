af5e7a6c272d8a98a95202ffa8035571e95c8759
tuhin637 <tuhin637bd@gmail.com>
2023-02-06 00:15:16 +0600

    3rd

---
+++ Lab Task 1/Name.java
@@ -0,0 +1,12 @@
+
+
+package com.mycompany.name;
+
+
+public class Name {
+
+    public static void main(String[] args) {
+     String Name =("Tuhinuzzaman Tuhin");
+        System.out.println(Name);
+    }
+}
