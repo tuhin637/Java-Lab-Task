d6ee2da03ab998dd45f206403441a7e04477adc4
tuhin637 <tuhin637bd@gmail.com>
2023-02-05 23:56:01 +0600

    1st

---
+++ Lab Task 1/Biodata.java
@@ -0,0 +1,16 @@
+
+
+package com.mycompany.biodata;
+
+public class Biodata {
+
+    public static void main(String[] args) {
+        System.out.println("Tuhinuzzaman Tuhin");
+        System.out.println("ID:221-15-4649");
+        System.out.println("Address:\"Ranisonkail\" Thakugaon");
+        System.out.println("Department Of CSE");
+        System.out.println("Favourite Game:Cricket");
+        
+       
+    }
+}
