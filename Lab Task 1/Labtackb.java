4c75787bd428311c15f31c432b1a57fa8ea2a557
tuhin637 <tuhin637bd@gmail.com>
2023-02-05 23:47:12 +0600

    2nd

--- Lab Task 1/Labtackb.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.labtackb;
-public class Labtackb {
-
-    public static void main(String[] args) {
-        System.out.println("Tuhinuzzaman Tuhin");
-    }
-}
--- Lab Task 1/SimpleCalculator.java
+++ Lab Task 1/SimpleCalculator.java
@@ -1,23 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.labtask1;
 
-/**
- *
- * @author USER
- */
+package com.mycompany.simplecalculator;
+
+
 public class SimpleCalculator {
-    public static void main(String ars[]){
+
+    public static void main(String[] args) {
+        
         int a,b;
-        a=10;
-        b=50;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(b/a);
-        System.out.println(a*b);
+        a=30;
+        b=10;
+        
+        int Summation = a+b;
+        int Subtraction = a-b;
+        int Multiplication = a*b;
+        int Division = a/b;
+        
+        System.out.println("Summation:" +Summation);
+        System.out.println("Subtraction:" +Subtraction);
+        System.out.println("Multiplication:" +Multiplication);
+        System.out.println("Division:" +Division);
     }
-    
-    
 }
