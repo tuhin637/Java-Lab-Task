620a99ba3fbbb87a8220fc3076f04434997d07d3
tuhin637 <tuhin637bd@gmail.com>
2023-02-06 00:16:57 +0600

    4rd

---
+++ Lab Task 1/AreaCircel.java
@@ -0,0 +1,15 @@
+
+
+package com.mycompany.areacircel;
+
+
+public class AreaCircel {
+
+    public static void main(String[] args) {
+      
+        double pi =3.1416,area;
+        int r=2;
+        area=pi*r*r;
+        System.out.printf("Area of Circle is : %.2f\n", area);
+    }
+}
